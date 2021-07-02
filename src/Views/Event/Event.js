60a481eebf8bd7d8ff9877a583c98592fb147baf
MrR3set <bairon.pazrobles@gmail.com>
2021-07-01 21:26:52 -0700

    added editable fields

--- src/Views/Event/Event.js
+++ src/Views/Event/Event.js
@@ -3,28 +3,35 @@ import './Event.scss';
 import axios from "axios";
 import {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
+import Table from "../../Components/Table/Table"
 
 function EventPage() {
 	// Here we get all the events...
 	const {id} = useParams()
 
 	const [event,setEvent] = useState({});
-	const [isNew, setIsNew] = useState(id === 'new')
-
+	const [isNew, setIsNew] = useState(id === 'new');
+	const [editing, setEditing] = useState(false);
+	const [addResults,setAddResults]=useState(true);
+	const [results,setResults]=useState([]);
 
 	useEffect(()=>{
 		if (!isNew && !Object.keys(event).length > 0)
 			axios.get(`http://localhost:5001/api/admin/events/${id}`).then(res=>{
 				setEvent(res.data);
-				console.log(res.data)
 			})
-		else
-			console.log("nah")
 	},[]);
+
+	useEffect(()=>{
+		if(event.results){
+			setResults(event.results[0])
+		}
+
+	},[event]);
+
 
 	const onChangeHandler = (e) => {
 		e.preventDefault();
-		console.log(e.target.name);
 		setEvent({...event, [e.target.name]:e.target.value});
 	}
 	
@@ -41,16 +48,41 @@ function EventPage() {
 		})
 	}
 
+	const saveResults = (newValue) => {
+		setResults(newValue);
+	}
+
+	const pushResults = (e) => {
+		e.preventDefault();
+
+		if(results.length > 0)
+			axios.post(`http://localhost:5001/api/admin/events/${id}`, {results:{0:results}}).then(res=>{
+				console.log(res)
+			})
+	}
+	
+
+	
 
 	return (
 		<div className="event-page">
 
-			{isNew?<>
-				<input name={"name"} placeholder={"name"} onChange={onChangeHandler}/>
-				<input name={"sport"} placeholder={"sport"} onChange={onChangeHandler}/>
-				<input name={"status"} placeholder={"status"} onChange={onChangeHandler}/>
-				<input name={"type"} placeholder={"type"} onChange={onChangeHandler}/>
-				<input name={"date"} placeholder={"date"} type="datetime-local" onChange={onChangeHandler}/>
+			{isNew || editing?<>
+			
+				<input name="name" placeholder="name" value={event.name} onChange={onChangeHandler}/>
+			
+				<input name="sport" placeholder="sport" value={event.sport} onChange={onChangeHandler}/>
+			
+				<input name="status" placeholder="status" value={event.status} onChange={onChangeHandler}/>
+			
+				<select name="type" onChange={onChangeHandler} defaultValue={event.type?event.type:"info"}>
+					<option value="info" disabled>Tipo de competicion</option>
+					<option value="bracket">Bracket</option>
+					<option value="table">Table</option>
+				</select>
+		
+				<input name="date" placeholder="date" type="datetime-local" value={String(event.date).slice(0,16)} onChange={onChangeHandler}/>
+			
 				<button onClick={handleSave}>S</button>
 			</>:<>
 				<h3>{event.name}</h3>
@@ -59,6 +91,29 @@ function EventPage() {
 				<div>{event.type}</div>
 				<button onClick={(e)=>{e.preventDefault(); setEditing(true)}}>E</button>
 			</>}
+
+
+
+			<button onClick={pushUpdate}>Actualizar informacion del evento</button>
+
+
+
+			<br></br>
+
+			<button onClick={pushResults}>Subir resultados</button> 
+
+			
+			
+			<button onClick={(e)=>{e.preventDefault(); setAddResults(true)}}>Añadir resultados</button>
+
+			{
+				addResults?
+				<div>					
+					<Table results={results} saveResults={saveResults}/>
+				</div>:null
+			}
+
+
 
 		</div>
 	);
