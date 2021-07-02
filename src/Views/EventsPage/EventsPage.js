5a9153538a3d4726f0357bd78f0251360b0c3d16
MrR3set <bairon.pazrobles@gmail.com>
2021-07-01 22:23:38 -0700

    updated layout

--- src/Views/EventsPage/EventsPage.js
+++ src/Views/EventsPage/EventsPage.js
@@ -16,24 +16,26 @@ function EventsPage() {
 	},[])
 
 	return (
-		<div className="events-page">
-			<table>
-				<thead>
-					<tr>
-						<th>Hora</th>
-						<th>Deporte</th>
-						<th>Nombre</th>
-						<th>Estado</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{events.map((event,index)=>{
-						return <EventPreview id={event.id} name={event.name} sport={event.sport} 
-							status={event.status} date={String(event.date).slice(0,16).replace("T", " ")} results={event.hasResults} key={index}/>
-					})}
-				</tbody>
-        	</table>
+		<div className="events-wrapper page">
+			<div className="table-wrapper">
+				<table>
+					<thead>
+						<tr>
+							<th>Hora</th>
+							<th>Deporte</th>
+							<th>Nombre</th>
+							<th>Estado</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{events.map((event,index)=>{
+							return <EventPreview id={event.id} name={event.name} sport={event.sport} 
+								status={event.status} date={String(event.date).slice(11,16).replace("T", " ")} results={event.hasResults} key={index}/>
+						})}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
