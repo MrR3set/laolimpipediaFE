2c931f5e6fae9adb9779b27963dbf2ce6383674c
MrR3set <bairon.pazrobles@gmail.com>
2021-07-07 16:33:02 -0700

    fixes

--- src/Views/HomePage/HomePage.js
+++ src/Views/HomePage/HomePage.js
@@ -4,11 +4,25 @@ import Carrousel from "../../Components/Carrousel/Carrousel";
 import { Link } from "react-router-dom";
 
 const HomePageData = [
-	{name:"Horarios y Resultados en directo", path:"/admin/eventos", imageUrl:"https://images.unsplash.com/photo-1569517282132-25d22f4573e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1433&q=80"},
-	{name:"Medallero", path:"/admin/medallero"},
-	{name:"Guia oficial la Olimpipedia JJ.OO. Tokio 2020", path:"/admin/guia"},
-	{name:"¿Como seguir los juegos con nosotros?", path:"/admin/directo"},
-	{name:"Contacto", path:"/admin/contacto"},
+	{
+		name:"Horarios y Resultados en directo", 
+		path:"/admin/eventos", 
+		imageUrl:"https://images.unsplash.com/photo-1569517282132-25d22f4573e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1433&q=80"
+	},
+	{
+		name:"Medallero", 
+		path:"/admin/medallero", 
+		imageUrl:"https://images.unsplash.com/photo-1511406471420-feeac25c74c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=699&q=80"
+	},
+	{
+		name:"Guia oficial la Olimpipedia JJ.OO. Tokio 2020", 
+		path:"/admin/guia"},
+	{
+		name:"¿Como seguir los juegos con nosotros?", 
+		path:"/admin/directo"},
+	{
+		name:"Contacto", 
+		path:"/admin/contacto"},
 ]
 
 
@@ -32,7 +46,7 @@ export default HomePage;
 const ShowCase = ({imageUrl,inverted=false,path="/", name=""}) => {
 	return (
 		<div className="showcase">
-			<img src={imageUrl} alt="Showcase" />
+			<img src={imageUrl} alt={name + " previsualizacion"} />
 			<div className={`content ${inverted?"inv":''}`}>
 				<Link to={path} className="link">
 					{name}
