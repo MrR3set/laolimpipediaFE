9f0ac314616a746926624b1b84d50b4d8d84fbc5
MrR3set <bairon.pazrobles@gmail.com>
2021-07-08 20:16:06 -0700

    init

--- src/Views/LivePage/LivePage.js
+++ src/Views/LivePage/LivePage.js
@@ -4,35 +4,68 @@ import axios from "axios";
 import { ReactComponent as AddIcon } from '../../Assets/PlusIconRounded.svg';
 import "./LivePage.scss";
 
-function EventPage({allowEdits=true}) {
+function EventPage({allowEdits=false}) {
 
 	const [links,setLinks] = useState([]);
+	const [isAdding, setIsAdding] = useState(false);
+	const [newLink,setNewLink] = useState({url:"",activeDate:new Date(Date.now()).toISOString().slice(0,19),title:"", description:""})
 
+	useEffect(()=>{
+		axios.get(`http://localhost:5001/api/admin/livelinks/`).then(res=>{
+			setLinks(res.data);
+		})
+	},[]);
 
+	const toggleAdding = () => {
+		setIsAdding(!isAdding)
+	}
 
-	const [newLink,setNewLink] = useState({url:"",date:"",title:""})
+	const onChangeHandler = (e) => {
+		e.preventDefault();
+		setNewLink({...newLink, [e.target.name]:e.target.value});
+	}
 
-	// useEffect(()=>{
-	// 	axios.get(`http://localhost:5001/api/admin/livelinks/`).then(res=>{
-	// 		setLinks(res.data);
-	// 	})
-	// },[]);
+	console.log()
+
+	const uploadLink = () => {
+		axios.post(`http://localhost:5001/api/admin/livelinks/`, newLink).then(res=>{
+			if(res.status===201)
+				setNewLink({url:"",activeDate:new Date(Date.now()).toISOString().slice(0,19),title:"", description:""})
+		})
+	}
+
+	const deleteLink = (id) => {
+		let confirmation = window.confirm("Confirmar")
+		if(confirmation)
+			axios.delete(`http://localhost:5001/api/admin/livelinks/${id}`).then(res=>{
+				console.log(res);
+			})
+	}
 
 	
 	return (
 		<div className="livePage-wrapper page">
+ 
+			{links.map(linkData=>{
+				return <LinkPreview data={linkData} deleteLink={deleteLink}/>
+			})} 
 
-			{/* {links.map(linkData=>{
-				return <LinkPreview data={linkData}/>
-			})} */}
+			{allowEdits?isAdding?
+				<div className="linkPreview-wrapper addLink Form">
+					<input name="title" placeholder="Titulo" value={newLink.title} onChange={onChangeHandler}/>
+					<input name="description" placeholder="Descripcion" maxLength={50} value={newLink.description} onChange={onChangeHandler}/>
+					<input name="url" placeholder="Link" type="url" value={newLink.url} onChange={onChangeHandler}/>
+					<input name="activeDate" placeholder="Date" type="datetime-local" value={newLink.activeDate} onChange={onChangeHandler}/>
 
-			<LinkPreview/>
+					<div className="controls">
+						<button onClick={uploadLink} className="cta">Guardar</button>
+						<button onClick={toggleAdding} className="cta">Cancelar</button>
+					</div>
 
-
-			<div className="linkPreview-wrapper newLink">
-				<AddIcon/>
-			</div>
-
+				</div>:<div className="linkPreview-wrapper addLink" onClick={toggleAdding}>
+					<AddIcon/>
+				</div>
+			:null}
 
 		</div>
 	);
