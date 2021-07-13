e809264d4cdb433e1febccca46577a9f58a85dae
MrR3set <bairon.pazrobles@gmail.com>
2021-07-12 23:41:44 -0700

    Fixes

--- src/Utils/PrivateRoute.js
+++ src/Utils/PrivateRoute.js
@@ -2,13 +2,16 @@ import React from "react";
 import {Route,Redirect} from "react-router-dom"
 
 const PrivateRoute = ({component:Component, authorized, path,...rest})=>{
+	console.log(authorized)
     return <Route
         {...rest}
         render={(props)=>{
             if(localStorage.getItem("token") && authorized)
                 return <Component {...props} allowEdits={authorized}/>
-            else
+            else if (String(path).split("/").includes("/admin") &&  !String(path).split("/").includes("/auth"))
                 return <Redirect to={String(path).replace("/admin", "")}/>
+			else
+				return <Redirect to={"/"}/>
         }}
     />
 }
