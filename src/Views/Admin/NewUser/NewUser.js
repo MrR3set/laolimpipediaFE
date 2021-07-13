e465e8c7ef48e49798f695278dc0818686ad530b
MrR3set <bairon.pazrobles@gmail.com>
2021-07-12 23:25:33 -0700

    Show created user information

--- src/Views/Admin/NewUser/NewUser.js
+++ src/Views/Admin/NewUser/NewUser.js
@@ -5,21 +5,28 @@ import './NewUser.scss';
 
 function NewUser() {
 
-	const [userInfo,setUserInfo] = useState({username:"", password:"", email:""});
+	const [userNewInfo,setNewUserInfo] = useState({username:"", password:"", email:""});
+	const [userData, setUserData] = useState("");
 	const [error,setError] = useState(null);
 
 	const changeHandler = (e) => {
 		e.preventDefault();
-		setUserInfo({...userInfo, [e.target.name]:e.target.value});
+		setNewUserInfo({...userNewInfo, [e.target.name]:e.target.value});
 	}
 
 	const createNewUser = () => {
 		if(!error){
-
-			axiosWithAuth().post("auth/register", {name: userInfo.name, password: userInfo.password, email: userInfo.email}).then(res => {
-				console.log(res);
+			axiosWithAuth().post("auth/register", userNewInfo).then(res => {
+				if(res.status===201){
+					console.log(res.data.username);
+					setUserData({username:res.data.username, password:userNewInfo.password});
+				}else if(res.status===202){
+					setError("Ese usuario ya existe");
+				}
 			}).catch(err=>{
-				console.log(err);
+				if(err.response && err.response.status && err.response.status===400){
+					setError("Todos lo campos son requieridos");
+				}
 			})
 		}
 	}
@@ -31,23 +38,29 @@ function NewUser() {
 		for (var i = 0, n = charset.length; i < length; ++i) {
 			nPass += charset.charAt(Math.floor(Math.random() * n));
 		}
-		setUserInfo({...userInfo, password:nPass})
+		setNewUserInfo({...userNewInfo, password:nPass})
 	}
 	
-
 	return (
-		<div className="login-wrapper page">
+		<div className="new-user-wrapper page">
 			<h1>Crear nuevo usuario</h1>
-			<div className="form">
+			{userData?<div className="user-info">
+				<p>Usuario: {userData.username}</p>
+				<p>Contraseña: {userData.password}</p>
+			</div>:<div className="form">
 				<input name="email" onChange={changeHandler} type="email" placeholder="Correo electronico"/>
 				<input name="username" onChange={changeHandler} type="text" placeholder="Usuario"/>
 				<div className="password">
-					<input name="password" onChange={changeHandler} type="text" value={userInfo.password} placeholder="Contraseña"/>
+					<input name="password" onChange={changeHandler} type="text" value={userNewInfo.password} placeholder="Contraseña"/>
 					<button className="cta" onClick={generatePassword}>Generar contraseña</button>
 				</div>
-				<button onClick={createNewUser}>Iniciar sesion</button>
+				<button onClick={createNewUser}>Crear usuario</button>
 				{error?<p>{error}</p>:null}
-			</div>
+			</div>}
+
+
+
+
 		</div>
 	);
 }
